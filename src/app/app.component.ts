9f03885b5691d270887728543b707087a4b1809d
ghimarcos <ghimarcos123@gmail.com>
2021-04-05 13:34:30 -0300

    usando o ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { noUndefined } from '@angular/compiler/src/util';
 import { Component, Input } from '@angular/core';
-import {NgbAlertConfig} from '@ng-bootstrap/ng-bootstrap';
+import { NgbAlertConfig } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -9,13 +9,22 @@ import {NgbAlertConfig} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    nome = "Giovani";
-    adicionado = false;
 
-    //evento adicionar que está no botão do html com metodo (click)
-    adicionar(){
+  ultimoId = 0;
+  nome = "Ana";
+  adicionado = false;
+  //cria um array para adicionar os elementos do *ngFor
+  funcionarios = [];
+  //evento adicionar que está no botão do html com metodo (click)
+  adicionar() {
     console.log(`Adicionando ${this.nome}`)
-      this.adicionado = true;
-    }
+    // muda o bolean para true;
+    this.adicionado = true;
+    //Adiona um novo elemento no array
+    this.funcionarios.push({
+      id: ++this.ultimoId,
+      nome : this.nome
+    });
+  }
 
 }
