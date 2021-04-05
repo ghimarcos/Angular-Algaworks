e8751d85c9025477f6d0cb794a254e5c5c3a086e
ghimarcos <ghimarcos123@gmail.com>
2021-04-05 10:09:01 -0300

    Usando uma variável de referencia para pegar valor da label

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,8 @@ export class AppComponent {
     nome = "Giovani";
 
     //evento adicionar que está no botão do html com metodo (click)
-    adicionar(){
-      //log mostrando no console "Adicionando" e concatenando o valor da variavel nome;
-      console.log(`Adicionando ${this.nome}`);
-
-      //Adiciona um numéro aleatório até 100
-      const numero = Math.round(Math.random() * 100) ;
-      //passando nome João + numero aleatório de 0 a 100
-      this.nome = 'João ' + numero
+    adicionar(nome : string){
+     this.nome = nome
     }
 
-    //evento alterar nome que está no input do html recebendo um "event"
-    alterarNome(event : any){
-      //imprimindo no console oque está sendo capturado no evento
-      console.log(event)
-      //dentro do método event busco o target.value, que trás exatamente o valor que está sendo digitado dentro da label
-      this.nome = event.target.value;
-    }
 }
