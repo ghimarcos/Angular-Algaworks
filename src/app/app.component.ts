f1b9b14ed850f87d95802716ea3391c2e16c87b3
ghimarcos <ghimarcos123@gmail.com>
2021-04-05 10:00:51 -0300

    Entendendo eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {NgbAlertConfig} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+    nome = "Giovani";
+
+    //evento adicionar que está no botão do html com metodo (click)
+    adicionar(){
+      //log mostrando no console "Adicionando" e concatenando o valor da variavel nome;
+      console.log(`Adicionando ${this.nome}`);
+
+      //Adiciona um numéro aleatório até 100
+      const numero = Math.round(Math.random() * 100) ;
+      //passando nome João + numero aleatório de 0 a 100
+      this.nome = 'João ' + numero
+    }
+
+    //evento alterar nome que está no input do html recebendo um "event"
+    alterarNome(event : any){
+      //imprimindo no console oque está sendo capturado no evento
+      console.log(event)
+      //dentro do método event busco o target.value, que trás exatamente o valor que está sendo digitado dentro da label
+      this.nome = event.target.value;
+    }
 }
